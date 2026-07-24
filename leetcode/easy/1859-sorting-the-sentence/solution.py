19c2eeacc784aa8b8e67ea1c5ac3f01670bff2ac
Rushikesh Markad <rushikeshmarkad465@gmail.com>
2026-07-24 13:45:20 +0530

    ✅ Sorting the Sentence | Easy | Python | Runtime: 36 ms | Memory: 19.3 MB | 2026-07-24

--- leetcode/easy/1859-sorting-the-sentence/solution.py
+++ leetcode/easy/1859-sorting-the-sentence/solution.py
@@ -1,4 +1,12 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         str_list = s.split()
-        return str_list
+        result = [""]*len(str_list)
+
+        for word in str_list:
+            n = len(word)-1
+            pos = int(word[n])
+
+            result[pos-1] = word[:n]
+
+        return result
