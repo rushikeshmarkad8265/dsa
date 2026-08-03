c0d2a8d3db7171ea5fe7bbe90328e8416680312b
Rushikesh Markad <rushikeshmarkad465@gmail.com>
2026-08-03 10:18:12 +0530

    ✅ The Report | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-03

--- hackerrank/medium/the-report/solution.sql
+++ hackerrank/medium/the-report/solution.sql
@@ -1,7 +1,16 @@
-SELECT co.Continent, FLOOR((AVG(c.Population)))
-FROM CITY AS c
-JOIN
-COUNTRY co
-ON
-c.CountryCode = co.code
-GROUP BY co.Continent
+/*
+Enter your query here.
+*/
+SELECT 
+    CASE 
+        WHEN Grades.Grade < 8 THEN 'NULL' 
+        ELSE Students.Name 
+    END AS StudentName,
+    Grades.Grade,
+    Students.Marks
+FROM 
+    Students, Grades 
+WHERE 
+    Students.Marks >= Grades.Min_mark AND Students.Marks <= Grades.Max_mark 
+ORDER BY 
+    Grades.Grade DESC, Students.Name;
