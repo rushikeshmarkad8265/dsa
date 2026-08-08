9a830d36a0c9f0dee18ed2f379da33dd070115ba
Rushikesh Markad <rushikeshmarkad465@gmail.com>
2026-08-08 16:05:59 +0530

    ✅ Check the status - Python | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-08

---
+++ gfg/easy/check-the-status/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkStatus(self, a, b, flag):
+        # code here
+        if (a < 0 and b < 0 and flag == True ) or (a < 0 and b > 0 and flag == False) or(a > 0 and b < 0 and flag == False):
+            return True
+        
+        return False
